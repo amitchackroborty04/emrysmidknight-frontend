9dee6a2e809735695384f9064c9805957b4e51df
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2026-03-31 10:35:51 +0600

    add

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from './_components/SignUpForm'
+
+function page() {
+  return (
+    <div>
+        <SignUpForm />
+    </div>
+  )
+}
+
+export default page
